9a5e648e6a8198de050184bfacb17f23cf988610
Nachyn <nachyn@outlook.com>
2020-03-15 18:18:19 +0700

    AppVm: Initialized SolidWorksSettings

---
+++ Drawer3D/Drawer3D.ViewWpf/ViewModels/ApplicationVm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using Drawer3D.Model;
+
+namespace Drawer3D.ViewWpf.ViewModels
+{
+    public class ApplicationVm
+    {
+        private Drawer _drawer;
+
+        public ApplicationVm()
+        {
+            InitializeDrawer();
+        }
+
+        private void InitializeDrawer()
+        {
+            _drawer = new Drawer(new FigureSettings(),
+                new SolidWorksCommander(GetSolidWorksSettings()));
+        }
+
+        private SolidWorksSettings GetSolidWorksSettings()
+        {
+            var name = Application.Current.Resources["SolidWorksName"].ToString();
+            var guid = Application.Current.Resources["SolidWorksGuid"].ToString();
+            return new SolidWorksSettings {Guid = new Guid(guid), Name = name};
+        }
+    }
+}
