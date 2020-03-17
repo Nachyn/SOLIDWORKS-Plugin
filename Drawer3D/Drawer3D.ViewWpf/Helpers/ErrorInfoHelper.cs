32ae8f7a67e1e2492ebbb340246697fc08a1c353
Nachyn <nachyn@outlook.com>
2020-03-17 16:43:17 +0700

    Added ErrorInfoHelper

---
+++ Drawer3D/Drawer3D.ViewWpf/Helpers/ErrorInfoHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Drawer3D.Model.Exceptions;
+
+namespace Drawer3D.ViewWpf.Helpers
+{
+    public static class ErrorInfoHelper
+    {
+        public static string HandleErrorInfo(Action action)
+        {
+            try
+            {
+                action();
+                return string.Empty;
+            }
+            catch (FigureException exception)
+            {
+                return exception.FigureError.Message;
+            }
+        }
+    }
+}
