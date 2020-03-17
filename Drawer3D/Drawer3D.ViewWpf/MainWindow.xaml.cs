a15feefbd3b775f2276a4e30bcdc17260d209a26
Nachyn <nachyn@outlook.com>
2020-03-17 16:51:36 +0700

    Added Figure Settings layout

--- Drawer3D/Drawer3D.ViewWpf/MainWindow.xaml.cs
+++ Drawer3D/Drawer3D.ViewWpf/MainWindow.xaml.cs
@@ -1,28 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Drawer3D.ViewWpf
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<BindingExpression> _bindingPointsX;
+
+        private readonly List<BindingExpression> _bindingPointsY;
+
         public MainWindow()
         {
             InitializeComponent();
+            _bindingPointsX = new List<BindingExpression>();
+            _bindingPointsY = new List<BindingExpression>();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            GetTextBinding(sender).UpdateSource();
+        }
+
+        private void TextBoxPointX_Loaded(object sender, RoutedEventArgs e)
+        {
+            _bindingPointsX.Add(GetTextBinding(sender));
+        }
+
+        private void TextBoxPointY_Loaded(object sender, RoutedEventArgs e)
+        {
+            _bindingPointsY.Add(GetTextBinding(sender));
+        }
+
+        private void TextBoxPointX_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _bindingPointsX.Remove(GetTextBinding(sender));
+        }
+
+        private void TextBoxPointY_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _bindingPointsY.Remove(GetTextBinding(sender));
+        }
+
+        private void TextBoxPointX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox_TextChanged(sender, e);
+            _bindingPointsX.ForEach(b => b.UpdateSource());
+        }
+
+        private void TextBoxPointY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox_TextChanged(sender, e);
+            _bindingPointsY.ForEach(b => b.UpdateSource());
+        }
+
+        private static BindingExpression GetTextBinding(object sender)
+        {
+            return ((TextBox) sender).GetBindingExpression(TextBox.TextProperty);
         }
     }
 }
