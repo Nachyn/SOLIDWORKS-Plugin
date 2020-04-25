de7a1daeca5590f67478a75ee9e481c1c6e18d25
Nachyn Duuza <nachyn@outlook.com>
2020-04-25 16:53:17 +0700

    Added Zoom to API Commander, fixed CreateBase with zoom in Drawer

--- Drawer3D/Drawer3D.Model/SolidWorksCommander.cs
+++ Drawer3D/Drawer3D.Model/SolidWorksCommander.cs
@@ -270,5 +270,38 @@ namespace Drawer3D.Model
 
             _document.EditDelete();
         }
+
+        /// <summary>
+        ///     Увеличить по координатам
+        /// </summary>
+        /// <param name="x1">1 координата по X</param>
+        /// <param name="y1">1 координата по Y</param>
+        /// <param name="z1">1 координата по Z</param>
+        /// <param name="x2">2 координата по X</param>
+        /// <param name="y2">2 координата по Y</param>
+        /// <param name="z2">2 координата по Z</param>
+        public void Zoom(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            if (!IsConnectedToApp)
+            {
+                return;
+            }
+
+            _document.ViewZoomTo2(x1.ToMilli(), y1.ToMilli(), z1.ToMilli()
+                , x2.ToMilli(), y2.ToMilli(), z2.ToMilli());
+        }
+
+        /// <summary>
+        ///     Увеличить фигуру в полный размер
+        /// </summary>
+        public void ZoomToFit()
+        {
+            if (!IsConnectedToApp)
+            {
+                return;
+            }
+
+            _document.ViewZoomtofit2();
+        }
     }
 }
